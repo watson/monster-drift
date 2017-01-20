1deb934aca862b9699cd7aa10becc3cb63fec57b
Mathias Buus <mathiasbuus@gmail.com>
2017-01-20 17:18:26 +0100

    add uturn to example

--- example.js
+++ example.js
@@ -9,6 +9,8 @@ keypress(process.stdin)
 process.stdin.on('keypress', function (ch, key) {
   if (key && key.ctrl && key.name === 'c') return exit()
 
+  clearTimeout(timer)
+
   switch (key.name) {
     case 'w': drive.forward(); break
     case 'q': drive.forwardRight(); break
@@ -16,12 +18,15 @@ process.stdin.on('keypress', function (ch, key) {
     case 's': drive.backward(); break
     case 'a': drive.backwardRight(); break
     case 'd': drive.backwardLeft(); break
+    case 'u': drive.uturn(stop)
   }
 
-  clearTimeout(timer)
-  timer = setTimeout(function () {
+  if (key.name === 'u') return // uturn stops for us
+  timer = setTimeout(stop, 150)
+
+  function stop () {
     drive.stop()
-  }, 150)
+  }
 })
 process.stdin.setRawMode(true)
 process.stdin.resume()
